9300c1dd1c3a81419009a8f9234f6025a2b27c41
Dave Syer <dsyer@pivotal.io>
2018-07-24 08:54:00 +0100

    An alternative approach to MVC using more native features

--- spring-cloud-function-web/src/main/java/org/springframework/cloud/function/web/flux/FunctionController.java
+++ spring-cloud-function-web/src/main/java/org/springframework/cloud/function/web/flux/FunctionController.java
@@ -17,11 +17,16 @@
 package org.springframework.cloud.function.web.flux;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,16 +34,23 @@ import org.reactivestreams.Publisher;
 
 import org.springframework.cloud.function.context.catalog.FunctionInspector;
 import org.springframework.cloud.function.context.message.MessageUtils;
+import org.springframework.cloud.function.json.JsonMapper;
 import org.springframework.cloud.function.web.flux.constants.WebRequestConstants;
 import org.springframework.cloud.function.web.flux.request.FluxFormRequest;
-import org.springframework.cloud.function.web.flux.request.FluxRequest;
+import org.springframework.cloud.function.web.util.HeaderUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 import reactor.core.publisher.Flux;
@@ -59,7 +71,11 @@ public class FunctionController {
 
 	private StringConverter converter;
 
-	public FunctionController(FunctionInspector inspector, StringConverter converter) {
+	private JsonMapper mapper;
+
+	public FunctionController(JsonMapper mapper, FunctionInspector inspector,
+			StringConverter converter) {
+		this.mapper = mapper;
 		this.inspector = inspector;
 		this.converter = converter;
 	}
@@ -70,8 +86,48 @@ public class FunctionController {
 
 	@PostMapping(path = "/**")
 	@ResponseBody
-	public ResponseEntity<Publisher<?>> post(WebRequest request,
-			@RequestBody FluxRequest<?> body) {
+	public Mono<ResponseEntity<Publisher<?>>> post(NativeWebRequest request,
+			@RequestBody(required = false) String body) {
+		Object function = request.getAttribute(WebRequestConstants.FUNCTION,
+				WebRequest.SCOPE_REQUEST);
+		if (function == null) {
+			function = request.getAttribute(WebRequestConstants.CONSUMER,
+					WebRequest.SCOPE_REQUEST);
+		}
+		if (!StringUtils.hasText(body)) {
+			return post(request, (List<?>) null);
+		}
+		body = body.trim();
+		Object input;
+		if (body.startsWith("[")) {
+			input = mapper.toList(body, inspector.getInputType(function));
+		}
+		else {
+			if (body.startsWith("{")) {
+				input = mapper.toSingle(body, inspector.getInputType(function));
+			}
+			else if (body.startsWith("\"")) {
+				input = body.substring(1, body.length() - 2);
+			}
+			else {
+				input = converter.convert(function, body);
+			}
+		}
+		return post(request, input);
+	}
+
+	private Mono<ResponseEntity<Publisher<?>>> post(NativeWebRequest request,
+			Object body) {
+		if (body instanceof List) {
+			return post(request, (List<?>) body);
+		}
+		request.setAttribute(WebRequestConstants.INPUT_SINGLE, true,
+				WebRequest.SCOPE_REQUEST);
+		return post(request, Collections.singletonList(body));
+	}
+
+	private Mono<ResponseEntity<Publisher<?>>> post(NativeWebRequest request,
+			List<?> body) {
 
 		@SuppressWarnings("unchecked")
 		Function<Publisher<?>, Publisher<?>> function = (Function<Publisher<?>, Publisher<?>>) request
@@ -84,35 +140,51 @@ public class FunctionController {
 
 		FluxFormRequest<?, ?> form = FluxFormRequest.from(request.getParameterMap());
 
+		Flux<?> flux = body == null ? form.flux() : Flux.fromIterable(body);
+		if (debug) {
+			flux = flux.log();
+		}
+		if (inspector.isMessage(function)) {
+			flux = messages(request, function==null ? consumer : function, flux);
+		}
 		if (function != null) {
-			Flux<?> flux = body.body() == null ? form.flux() : body.flux();
-			if (debug) {
-				flux = flux.log();
-			}
 			Flux<?> result = Flux.from(function.apply(flux));
+			BodyBuilder builder = ResponseEntity.ok();
 			if (inspector.isMessage(function)) {
-				result = result.map(message -> MessageUtils.unpack(function, message));
+				result = result
+						.doOnNext(value -> addHeaders(builder, (Message<?>) value));
+				result = result.map(
+						message -> MessageUtils.unpack(function, message).getPayload());
 			}
 			if (logger.isDebugEnabled()) {
 				logger.debug("Handled POST with function");
 			}
-			return ResponseEntity.ok().body(response(request, function, single, result));
+			Publisher<?> response = response(request, function, single, result);
+			return Flux.from(response)
+					.then(Mono.fromSupplier(() -> builder.body(response)));
 		}
 
 		if (consumer != null) {
-			Flux<?> flux = body.body() == null ? form.flux().cache()
-					: body.flux().cache(); // send a copy back to the caller
-			if (debug) {
-				flux = flux.log();
-			}
 			consumer.accept(flux);
 			if (logger.isDebugEnabled()) {
 				logger.debug("Handled POST with consumer");
 			}
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(flux);
+			return Mono.just(ResponseEntity.status(HttpStatus.ACCEPTED).build());
 		}
 
 		throw new IllegalArgumentException("no such function");
+	}
+
+	private Flux<?> messages(NativeWebRequest request, Object function, Flux<?> flux) {
+		Map<String, Object> headers = HeaderUtils.fromHttp(new ServletServerHttpRequest(
+				request.getNativeRequest(HttpServletRequest.class)).getHeaders());
+		flux = flux.map(payload -> MessageUtils.create(function, payload, headers));
+		return flux;
+	}
+
+	private void addHeaders(BodyBuilder builder, Message<?> message) {
+		HttpHeaders headers = new HttpHeaders();
+		builder.headers(HeaderUtils.fromMessage(message.getHeaders(), headers));
 	}
 
 	private Publisher<?> response(WebRequest request, Object handler, Boolean single,
--- spring-cloud-function-web/src/main/java/org/springframework/cloud/function/web/flux/ReactorAutoConfiguration.java
+++ spring-cloud-function-web/src/main/java/org/springframework/cloud/function/web/flux/ReactorAutoConfiguration.java
@@ -16,34 +16,21 @@
 
 package org.springframework.cloud.function.web.flux;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.cloud.function.context.catalog.FunctionInspector;
-import org.springframework.cloud.function.json.JsonMapper;
-import org.springframework.cloud.function.web.flux.request.FluxHandlerMethodArgumentResolver;
-import org.springframework.cloud.function.web.flux.response.FluxReturnValueHandler;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.method.support.AsyncHandlerMethodReturnValueHandler;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import reactor.core.publisher.Flux;
 
@@ -59,9 +46,6 @@ import reactor.core.publisher.Flux;
 @AutoConfigureAfter({ JacksonAutoConfiguration.class, GsonAutoConfiguration.class })
 public class ReactorAutoConfiguration {
 
-	@Autowired
-	private ApplicationContext context;
-
 	@Bean
 	public FunctionHandlerMapping functionHandlerMapping(FunctionCatalog catalog,
 			FunctionController controller) {
@@ -75,44 +59,6 @@ public class ReactorAutoConfiguration {
 		return new BasicStringConverter(inspector, beanFactory);
 	}
 
-	@Configuration
-	protected static class FluxReturnValueConfiguration {
-		@Bean
-		public FluxReturnValueHandler fluxReturnValueHandler(FunctionInspector inspector,
-				HttpMessageConverters converters) {
-			return new FluxReturnValueHandler(inspector, converters.getConverters());
-		}
-	}
-
-	@Configuration
-	protected static class FluxArgumentResolverConfiguration {
-		@Bean
-		public FluxHandlerMethodArgumentResolver fluxHandlerMethodArgumentResolver(
-				FunctionInspector inspector, JsonMapper mapper) {
-			return new FluxHandlerMethodArgumentResolver(inspector, mapper);
-		}
-	}
-
-	@Bean
-	public SmartInitializingSingleton fluxRequestMappingHandlerAdapterProcessor(
-			RequestMappingHandlerAdapter adapter,
-			FluxHandlerMethodArgumentResolver resolver) {
-		return new SmartInitializingSingleton() {
-
-			@Override
-			public void afterSingletonsInstantiated() {
-				List<HandlerMethodArgumentResolver> resolvers = new ArrayList<>(
-						adapter.getArgumentResolvers());
-				resolvers.add(0, resolver);
-				adapter.setArgumentResolvers(resolvers);
-				List<HandlerMethodReturnValueHandler> handlers = new ArrayList<>(
-						adapter.getReturnValueHandlers());
-				handlers.add(0, context.getBean(FluxReturnValueHandler.class));
-				adapter.setReturnValueHandlers(handlers);
-			}
-
-		};
-	}
 
 	private static class BasicStringConverter implements StringConverter {
 
